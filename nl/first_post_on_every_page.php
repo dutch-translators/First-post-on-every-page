17c6ac0fb165e364db76b521c8050a7a4ebbe9fe
BianCionaa <michelheusschen@gmail.com>
2014-11-07 15:58:06 +0100

    Update first_post_on_every_page.php

--- nl/first_post_on_every_page.php
+++ nl/first_post_on_every_page.php
@@ -31,5 +31,5 @@ if (empty($lang) || !is_array($lang))
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 $lang = array_merge($lang, array(
-	'FIRST_POST_SHOW'		=> 'Show on top of every page',
+	'FIRST_POST_SHOW'		=> 'Toon bovenaan elke pagina',
 ));
